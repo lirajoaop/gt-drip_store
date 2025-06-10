9e1ec7dbab95579c142546bd8c7dfc87ea551fa2
lirajoaop <joaopedroababa132@gmail.com>
2025-06-09 23:02:12 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <div>
+        <p className='bg-red-500 text-emerald-500'>
+          brtt
+        </p>
+      </div>
+  )
+}
+
+export default App
